85a0c12803d614f25fed09140c0b0278c8e66583
Arshdeep2222 <arshkhaira25@gmail.com>
2018-09-21 17:25:51 -0400

    Initial commit

---
+++ lab 1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab_1.Startup))]
+namespace lab_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
